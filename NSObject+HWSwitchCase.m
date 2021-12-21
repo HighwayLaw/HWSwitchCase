c791f778130b68c01412269f087e498885d277d6
HighwayLaw <495255019@qq.com>
2021-12-21 10:45:25 +0800

    fix: fix the precision of NSNumber case

--- NSObject+HWSwitchCase.m
+++ NSObject+HWSwitchCase.m
@@ -7,6 +7,9 @@
 
 #import "NSObject+HWSwitchCase.h"
 
+const double  eps = 1e-8;
+#define FEQUAL(a,b) (fabs((a) - (b)) < eps)
+
 @implementation NSObject (HWSwitchCase)
 
 - (void)switchInCases:(NSDictionary<id<NSCopying>,HWCaseBlock> *)cases {
@@ -15,11 +18,34 @@
 
 - (void)switchInCases:(NSDictionary<id<NSCopying>,HWCaseBlock> *)cases withDefault:(HWCaseBlock)defaultBlock {
     NSAssert([self conformsToProtocol:@protocol(NSCopying)], @"must confirm to <NSCopying>");
-    HWCaseBlock caseBlock = cases[(id<NSCopying>)self];
+    HWCaseBlock caseBlock = nil;
+    
+    if ([self isKindOfClass:[NSNumber class]]) {
+        NSNumber *numKey = (NSNumber *)self;
+        NSNumber *numInDic = [self findEquivalentNumber:numKey inDictionary:cases];
+        caseBlock = cases[numInDic];
+    } else {
+        caseBlock = cases[(id<NSCopying>)self];
+    }
+    
     if (caseBlock) {
         caseBlock();
     } else if (defaultBlock) {
         defaultBlock();
     }
 }
+
+- (NSNumber *)findEquivalentNumber:(NSNumber *)num inDictionary:(NSDictionary *)dic {
+    double doubleKey = [num doubleValue];
+    for (NSNumber *numIndic in dic.allKeys) {
+        if ([numIndic isKindOfClass:[NSNumber class]]) {
+            double doubleIndic = [numIndic doubleValue];
+            if (FEQUAL(doubleIndic, doubleKey)) {
+                return numIndic;
+            }
+        }
+    }
+    return nil;
+}
+
 @end
